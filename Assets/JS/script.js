521b682f0433007fea751a32c17526ce7788712f
Racheal-spec <folorunshotosin334@gmail.com>
2020-11-04 05:08:08 -0800

    made few changes to the contact section

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,3 +1,4 @@
+
 gsap.registerPlugin(ScrollTrigger);
 
 let sections = gsap.utils.toArray(".panel");
@@ -16,3 +17,31 @@ gsap.to(sections, {
     end: () => "+=" + document.querySelector('.wrapper').offsetWidth
   }
 });
+
+let tl = gsap.timeline({
+  //delay: 0.5,
+  scrollTrigger: {
+    trigger:'.panel',
+    //start: "bottom center",
+  }, 
+});
+
+tl.from('.logo', {opacity: 0 , duration: 1, x: -50});
+tl.from('.video-content h1', {opacity: 0, duration: 0.8, y: -60 });
+tl.from('.video-content p', {opacity: 0 , duration: 1, x: -200});
+tl.from('.btn', {opacity: 0 , duration: 1, ease: "power3.out", y: 200});
+//tl.from('.about-section img', {opacity: 0, y: 100, duration: 1.5} )
+//tl.from('.about-text', {opacity: 0, y: 300, duration: 1} )
+
+tl.from('.about-section img', {
+  opacity: 1,
+  x: 100,
+  duration: 1,
+  ease: "power2.out"
+})
+tl.from('.about-text', {
+  opacity: 1,
+  y: -50,
+  duration: 1,
+  ease: "power2.out"
+})
