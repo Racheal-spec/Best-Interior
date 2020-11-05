e797766eb76300afa64e0ebc21b71ac3420d51f6
Racheal-spec <folorunshotosin334@gmail.com>
2020-11-05 08:31:29 -0800

    redesigned home page

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,4 +1,5 @@
 
+
 gsap.registerPlugin(ScrollTrigger);
 
 let sections = gsap.utils.toArray(".panel");
@@ -27,9 +28,9 @@ let tl = gsap.timeline({
 });
 
 tl.from('.logo', {opacity: 0 , duration: 1, x: -50});
-tl.from('.video-content h1', {opacity: 0, duration: 0.8, y: -60 });
-tl.from('.video-content p', {opacity: 0 , duration: 1, x: -200});
-tl.from('.btn', {opacity: 0 , duration: 1, ease: "power3.out", y: 200});
+tl.from('.hero-text h1', {opacity: 0, duration: 0.8, y: -60 });
+tl.from('.hero-text p', {opacity: 0 , duration: 1, x: -200});
+tl.from('.watch-btn', {opacity: 0 , duration: 1, ease: "power3.out", y: 200});
 //tl.from('.about-section img', {opacity: 0, y: 100, duration: 1.5} )
 //tl.from('.about-text', {opacity: 0, y: 300, duration: 1} )
 
@@ -44,4 +45,13 @@ tl.from('.about-text', {
   y: -50,
   duration: 1,
   ease: "power2.out"
+})
+
+/****Home Page animation */
+ let watchBtn = document.querySelector('.watch-btn');
+watchBtn.addEventListener('click', function(){
+
+  console.log('watched');
+  TweenMax.to('.video-col', 1 , {scaleY: 1, height: '100vh', ease: Circ.easeOut });
+  TweenMax.to('.box', 2 , {opacity: 1, scale:1, ease: Elastic.easeOut, delay: 1})
 })
