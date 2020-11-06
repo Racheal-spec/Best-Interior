4edaae22262ec31ee2ce621acdcef3165dc06ff6
Racheal-spec <folorunshotosin334@gmail.com>
2020-11-06 04:23:36 -0800

    fixed 414px screen resolution

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,5 +1,4 @@
 
-
 gsap.registerPlugin(ScrollTrigger);
 
 let sections = gsap.utils.toArray(".panel");
@@ -10,7 +9,6 @@ gsap.to(sections, {
   ease: "none",
   scrollTrigger: {
     trigger: ".wrapper",
-    //invalidateOnRefresh: true,
     pin: true,
     scrub: 1,
     snap: 1 / (sections.length - 1),
@@ -19,11 +17,12 @@ gsap.to(sections, {
   }
 });
 
+
+/****Home Page animation */
+
 let tl = gsap.timeline({
-  //delay: 0.5,
   scrollTrigger: {
     trigger:'.panel',
-    //start: "bottom center",
   }, 
 });
 
@@ -31,27 +30,12 @@ tl.from('.logo', {opacity: 0 , duration: 1, x: -50});
 tl.from('.hero-text h1', {opacity: 0, duration: 0.8, y: -60 });
 tl.from('.hero-text p', {opacity: 0 , duration: 1, x: -200});
 tl.from('.watch-btn', {opacity: 0 , duration: 1, ease: "power3.out", y: 200});
-//tl.from('.about-section img', {opacity: 0, y: 100, duration: 1.5} )
-//tl.from('.about-text', {opacity: 0, y: 300, duration: 1} )
 
-tl.from('.about-section img', {
-  opacity: 1,
-  x: 100,
-  duration: 1,
-  ease: "power2.out"
-})
-tl.from('.about-text', {
-  opacity: 1,
-  y: -50,
-  duration: 1,
-  ease: "power2.out"
-})
 
-/****Home Page animation */
+
  let watchBtn = document.querySelector('.watch-btn');
 
 watchBtn.addEventListener('click', function(){
   TweenMax.to('.video-col', 1 , {scaleY: 1, height: '100vh', ease: Circ.easeOut });
-  TweenMax.to('.box', 2 , {opacity: 1, scale:1, ease: Elastic.easeOut, delay: 1})
   TweenMax.to('.video-box', 2 , {opacity: 1, ease: Power2.easeOut, delay: 1})
 })
