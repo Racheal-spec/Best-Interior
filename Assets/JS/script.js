430706397e842d52d4c17b36747086b7d139ab22
Racheal-spec <folorunshotosin334@gmail.com>
2020-11-07 15:00:00 -0800

    revamped the home page and also added a video play button

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,4 +1,5 @@
 
+//Horizontal scrolling animation using gsap scrolltrigger plugin
 gsap.registerPlugin(ScrollTrigger);
 
 let sections = gsap.utils.toArray(".panel");
@@ -31,11 +32,55 @@ tl.from('.hero-text h1', {opacity: 0, duration: 0.8, y: -60 });
 tl.from('.hero-text p', {opacity: 0 , duration: 1, x: -200});
 tl.from('.watch-btn', {opacity: 0 , duration: 1, ease: "power3.out", y: 200});
 
+tl.to('.img-col', {scaleY: 1, duration: 1, height: '100vh', ease: Circ.easeOut });
+tl.to('.img-box', {opacity: 1, duration: 2, ease: Power2.easeOut, delay: 1})
 
+//video played on click on homepage
+const watchBtn = document.querySelector('.watch-btn');
+const videoContainer = document.querySelector('.video-container');
+const videoPlay = document.querySelector('video');
+const closeBtn = document.querySelector('.close');
 
- let watchBtn = document.querySelector('.watch-btn');
-
-watchBtn.addEventListener('click', function(){
-  TweenMax.to('.video-col', 1 , {scaleY: 1, height: '100vh', ease: Circ.easeOut });
-  TweenMax.to('.video-box', 2 , {opacity: 1, ease: Power2.easeOut, delay: 1})
+watchBtn.addEventListener('click', ()=> {
+  videoContainer.classList.toggle('active');
+  videoPlay.pause();
+  videoPlay.currentTime = 0;
 })
+
+closeBtn.addEventListener('click', ()=> {
+  videoContainer.classList.toggle('active');
+})
+//navigation button code
+
+const navigation = document.querySelector('.navigation');
+const eachSection = document.querySelectorAll('.panel');
+const navigationList = document.querySelectorAll('.navigation li');
+ const windowHeight = window.innerHeight;
+
+ //A function that removes the selected class and disables the scale property
+function reset() {
+  for(var i = 0; i < navigation.children.length; i++){
+    navigation.children[i].classList.remove('selected');
+  }
+}
+
+window.addEventListener('scroll', function(){
+  const scrollLeft = window.scrollY;
+ 
+  eachSection.forEach(function(section, i){
+    //offsetLeft returns the left position of an object including the padding
+if(section.offsetLeft < scrollLeft + windowHeight/2 && scrollLeft < section.offsetLeft + windowHeight/2){
+  reset();
+  navigation.children[i].classList.add('selected');
+}
+  });
+});
+
+navigationList.forEach(function(item, i){
+  item.addEventListener('click', function(){
+    window.scrollTo({
+      top: i * windowHeight,
+      behavior: 'smooth'
+    })
+  })
+});
